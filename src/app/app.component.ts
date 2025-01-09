dcc745080fbc059f657010140afffa60ad4aea66
Cambier-Adrien <adrien.cambier04@gmail.com>
2025-01-10 00:29:36 +0100

    {~} Modified dark-button style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { RouterModule } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "./components/header/header.component";
+import { RouterModule } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [RouterOutlet, HeaderComponent, RouterModule],
   template: `
-    <app-header/>
-    <main class="min-h-screen pt-16 p-8 bg-neutral-100 dark:bg-neutral-950">
+    <app-header />
+    <main class="min-h-screen pt-16 p-8 bg-neutral-100 dark:bg-slate-950">
       <router-outlet />
     </main>
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'Card List';
+  title = "Card List";
 }
