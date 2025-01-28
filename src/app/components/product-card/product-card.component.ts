50a71c9e1f7cc7b7971041fb002cc6bbca7688e1
Cambier-Adrien <adrien.cambier04@gmail.com>
2025-01-28 11:49:46 +0100

    {~} Corrected date format to french one

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -3,10 +3,16 @@ import { Product } from "../../interfaces/product";
 import { CommonModule } from "@angular/common";
 import { DarkButtonComponent } from "../dark-button/dark-button.component";
 import { ProductService } from "../../services/product.service";
+import { registerLocaleData } from "@angular/common";
+import localeFr from "@angular/common/locales/fr"; // Importation de la locale française
+import { LOCALE_ID } from "@angular/core";
+
+registerLocaleData(localeFr, "fr");
 
 @Component({
   selector: "app-product-card",
   imports: [CommonModule, DarkButtonComponent],
+  providers: [{ provide: LOCALE_ID, useValue: "fr" }], // Définition de la locale française
   templateUrl: "./product-card.component.html",
   styles: ``,
 })
